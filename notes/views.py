287c157d939715bbce7a86bd2fa937fa3c77ca06
Alok3k7 <aloknathtiwari2000@gmail.com>
2024-08-12 17:54:09 +0530

    path update in notes file

--- notes/views.py
+++ notes/views.py
@@ -13,19 +13,19 @@ from .models import Notes
 class NotesDeleteView(DeleteView):
     model = Notes
     success_url = '/smart/notes'
-    template_name = '/home/alok/Desktop/smartnotes/notes/templates/notes_delete.html'
+    template_name = 'notes/templates/notes_delete.html'
 
 class NotesUpdateView(UpdateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
-    template_name = '/home/alok/Desktop/smartnotes/notes/templates/notes_form.html'
+    template_name = 'notes/templates/notes_form.html'
 
 class NotesCreateView(LoginRequiredMixin, CreateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
-    template_name = '/home/alok/Desktop/smartnotes/notes/templates/notes_form.html'
+    template_name = 'notes/templates/notes_form.html'
     
     def form_valid(self, form):
         self.object = form.save(commit=False)
@@ -36,7 +36,7 @@ class NotesCreateView(LoginRequiredMixin, CreateView):
 class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = 'notes'
-    template_name = '/home/alok/Desktop/smartnotes/notes/templates/notes_list.html'
+    template_name = 'notes/templates/notes_list.html'
     login_url = '/admin'
     
     def get_queryset(self):
@@ -45,4 +45,4 @@ class NotesListView(LoginRequiredMixin, ListView):
 class NotesDetailView(DetailView):
     model = Notes
     context_object_name = 'note'
-    template_name = '/home/alok/Desktop/smartnotes/notes/templates/notes_detail.html'
+    template_name = 'notes/templates/notes_detail.html'
